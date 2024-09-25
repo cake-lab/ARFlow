b0dbe1fd3cee5bb344cc52b0b9d799d202981316
felixng <ngthinh302@gmail.com>
2024-09-24 21:21:29 -0400

    feat(server): proper graceful shutdown handling

--- python/arflow/serve.py
+++ python/arflow/serve.py
@@ -4,6 +4,8 @@ import logging
 import sys
 from concurrent import futures
 from pathlib import Path
+from signal import SIGINT, SIGTERM, signal
+from typing import Any
 
 import grpc
 
@@ -13,24 +15,37 @@ from arflow.core import ARFlowServicer
 
 def create_server(port: int = 8500, path_to_save: Path | None = None):
     """Run gRPC server."""
-    try:
-        servicer = ARFlowServicer()
-        server = grpc.server(
-            futures.ThreadPoolExecutor(max_workers=10),
-            options=[
-                ("grpc.max_send_message_length", -1),
-                ("grpc.max_receive_message_length", -1),
-            ],
-        )
-        service_pb2_grpc.add_ARFlowServicer_to_server(servicer, server)
-        server.add_insecure_port("[::]:%s" % port)
-        server.start()
-        print(f"ARFlow server started, listening on {port}")
-        server.wait_for_termination()
-    except KeyboardInterrupt:
+    servicer = ARFlowServicer()
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10),
+        options=[
+            ("grpc.max_send_message_length", -1),
+            ("grpc.max_receive_message_length", -1),
+        ],
+    )
+    service_pb2_grpc.add_ARFlowServicer_to_server(servicer, server)
+    server.add_insecure_port("[::]:%s" % port)
+    server.start()
+    print(f"ARFlow server started, listening on {port}")
+
+    def handle_shutdown(*_: Any) -> None:
+        """Shutdown gracefully."""
+        print("Received shutdown signal")
+        # shutdows gracefully, refuse new requests, and wait for all RPCs to finish
+        # returns a `threading.Event` object on which to wait
+        all_rpcs_done_event = server.stop(30)
+        # block/wait the object so Python doesn't exit prematurely
+        all_rpcs_done_event.wait(30)
+
         if path_to_save is not None:
+            print("Saving data...")
             servicer._on_program_exit(path_to_save)
-        sys.exit(0)
+
+        print("Shut down gracefully")
+
+    signal(SIGTERM, handle_shutdown)  # request to terminate the process
+    signal(SIGINT, handle_shutdown)  # request to interrupt the process
+    server.wait_for_termination()
 
 
 def serve():
