2d99893a850bd7c1d80b50cdd3d3adcb64f84b6e
Thinh Nguyen <ngthinh302@gmail.com>
2024-09-25 22:01:19 -0400

    fix(client): sync new location of gRPC schema

--- protos/compile.sh
+++ protos/compile.sh
@@ -4,4 +4,4 @@
 python -m grpc_tools.protoc --proto_path=protos --python_out=python --pyi_out=python --grpc_python_out=python protos/arflow_grpc/*.proto
 
 # Generate C# gRPC files
-protoc --csharp_out=unity/Assets/Scripts/ARFlow --grpc_csharp_out=unity/Assets/Scripts/ARFlow protos/arflow_grpc/*.proto
+protoc --csharp_out=unity/Assets/Scripts/ARFlow --grpc_out=unity/Assets/Scripts/ARFlow --plugin=protoc-gen-grpc=grpc_csharp_plugin protos/arflow_grpc/*.proto
