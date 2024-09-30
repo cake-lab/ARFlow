441490f6d05f17b5ef42d3938f39e2b0c01fa199
Khang Luu <lnntkhang@gmail.com>
2024-09-30 16:32:16 -0400

    Chore: Remove redundant method

--- python/arflow/core.py
+++ python/arflow/core.py
@@ -329,26 +329,3 @@ class ARFlowService(service_pb2_grpc.ARFlowService):
         clr = color_rgb.reshape(-1, 3)
 
         return pcd, clr
-
-    @staticmethod
-    def euler_from_quaternion(x, y, z, w):
-        """
-        Convert a quaternion into euler angles (roll, pitch, yaw)
-        roll is rotation around x in radians (counterclockwise)
-        pitch is rotation around y in radians (counterclockwise)
-        yaw is rotation around z in radians (counterclockwise)
-        """
-        t0 = +2.0 * (w * x + y * z)
-        t1 = +1.0 - 2.0 * (x * x + y * y)
-        roll_x = np.arctan2(t0, t1)
-
-        t2 = +2.0 * (w * y - z * x)
-        t2 = +1.0 if t2 > +1.0 else t2
-        t2 = -1.0 if t2 < -1.0 else t2
-        pitch_y = np.arcsin(t2)
-
-        t3 = +2.0 * (w * z + x * y)
-        t4 = +1.0 - 2.0 * (y * y + z * z)
-        yaw_z = np.arctan2(t3, t4)
-
-        return roll_x, pitch_y, yaw_z  # in radians
