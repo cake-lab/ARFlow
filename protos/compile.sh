8ffc7378e978980ab8f0a7850951e2cc2e2aa2f5
felixngfender <ngthinh302@gmail.com>
2024-10-21 11:05:55 -0400

    chore: centralize build script

--- protos/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Generate Python gRPC files
-python -m grpc_tools.protoc --proto_path=protos --python_out=python --pyi_out=python --grpc_python_out=python protos/arflow_grpc/*.proto
-
-# Generate C# gRPC files
-protoc --csharp_out=unity/Assets/Scripts/ARFlow --grpc_out=unity/Assets/Scripts/ARFlow --plugin=protoc-gen-grpc=grpc_csharp_plugin protos/arflow_grpc/*.proto
-
-# protoc --csharp_out=unity/Assets/Scripts/ARFlow --grpc_out=unity/Assets/Scripts/ARFlow --plugin=protoc-gen-grpc=./.grpc_tools/grpc_csharp_plugin.exe protos/arflow_grpc/*.proto
