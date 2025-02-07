a40397becaf6a14e1e8c2e1b0197a07b2b35164a
Khang Luu <lnntkhang@gmail.com>
2025-02-07 11:35:20 -0500

    chore(wip): evaluations for spacial/coordinates syncronization

---
+++ unity/Packages/edu.wpi.cake.arflow/Runtime/Evaluation/SpacialEvaluation.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace CakeLab.ARFlow.Evaluation
+{
+    using Codice.Client.Common.FsNodeReaders;
+    using PlasticGui.WorkspaceWindow;
+
+    /// <summary>
+    /// Class to evaluate the spacial accuracy of the ARFlow system.
+    /// 
+
+    public class SpacialEvaluation
+    {
+        /// <summary>
+        /// Calculate the Frobenius norm of the difference between the ground truth and the obtained pose
+        /// Matrix4x4 does not have a built-in matrix operation for adding, so we will manually calculate the Frobenius norm
+        /// </summary>
+        /// <param name="groundTruth">Ground truth pose data in transformation matrix</param>
+        /// <param name="obtainedPose">Predicted pose data from the current method</param>
+        /// <returns></returns>
+        // public abstract Matrix4x4 ObtainPoseFromImage(Texture2D capturedImage, float markerLength, Mat camIntrinsics);
+        public static float GetErrorScore(Matrix4x4 groundTruth, Matrix4x4 obtainedPose)
+        {
+            float errorScore = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    float difference = groundTruth[i, j] - obtainedPose[i, j];
+                    errorScore += difference * difference;
+                }
+            }
+
+            return Mathf.Sqrt(errorScore);
+        }
+    }
+}
