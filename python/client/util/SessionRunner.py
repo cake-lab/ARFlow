2aa005075b40dda34894783b19a2d8df611c2255
Nikita Ostapenko <nikitul2005@gmail.com>
2025-07-10 00:12:30 -0400

    add vedant saran's test and modify it to test for frame-by-frame

--- python/client/util/SessionRunner.py
+++ python/client/util/SessionRunner.py
@@ -14,6 +14,8 @@ class SessionRunner:
     camera : cv2.VideoCapture | None = None
     session: Session | None = None
     device: Device | None = None
+    frames_gathered: int = 0
+    summed_psnr: float = 0.0
     onARFrame: Callable[[Session, ARFrame, Device], Coroutine[Any, Any, None]] | None = None
     def __init__(self, session: Session, device: Device, onARFrame: Callable[[Session, ARFrame, Device], Coroutine[Any, Any, None]]):
         self.camera = cv2.VideoCapture(0)
@@ -31,24 +33,35 @@ class SessionRunner:
         if not ret:
             return
         height, width = frame.shape[:2]
-        success, encoded = cv2.imencode('.jpg', frame)
+        
+        success, encoded = cv2.imencode('.png', frame)
         if not success:
             return
+        self.summed_psnr += cv2.PSNR(frame, cv2.imdecode(encoded, cv2.IMREAD_COLOR))
         plane: XRCpuImage.Plane = XRCpuImage.Plane(
             data=encoded.tobytes(),
             row_stride=0,
             pixel_stride=0,
         )
+        """
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        plane: XRCpuImage.Plane = XRCpuImage.Plane(
+            data=frame_rgb.tobytes(),
+            row_stride=3 * width,
+            pixel_stride=3,
+        )
+        """
         now = time.time()
         timestamp = Timestamp()
         nanos = int(now * 1e9)
         Timestamp.FromNanoseconds(timestamp, nanos)
         xrcpu_image: XRCpuImage = XRCpuImage(
             dimensions= Vector2Int(x=width, y=height),
-            format= XRCpuImage.Format.FORMAT_JPEG_RGB24,
+            format= XRCpuImage.Format.FORMAT_PNG_RGB24,
             timestamp=now,
             planes=[plane]
         )
+        
         color_frame = ColorFrame(
             image=xrcpu_image,
             device_timestamp=timestamp
